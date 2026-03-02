ee96deebf25d1ddb0ef9b4e2c0516bce196bd73d
Ankita Ray <rayankita181000@gmail.com>
2026-03-02 11:40:48 +0530

    rent updated

--- src/pages/rent/sections/RentForm.jsx
+++ src/pages/rent/sections/RentForm.jsx
@@ -48,9 +48,10 @@ export default function RentForm({ formData, setFormData, onSubmit }) {
           Find Your <span className="italic text-orange-500">Next Space</span>
         </h2>
         <p className="text-slate-500 max-w-md text-xs lg:text-base leading-relaxed font-medium">
-          Property wala bhaiya free brings focus onto reality by connecting
-          owners, buyers and tenants through a single platform. It is powered by java technology.
-          Verified listings with complete legal due diligence.
+          Property Wala simplifies real estate by connecting property owners,
+          buyers, and tenants on one trusted platform. Powered by Java
+          technology, we ensure verified listings backed by complete legal due
+          diligence for a safe and transparent property experience.
         </p>
       </div>
 
