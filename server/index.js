75d5491d2721b778ead5f39de4a528441227fac8
Paras Kanjiya <kanjiyaparas2002@gmail.com>
2026-06-05 11:51:48 +0530

    chore: configure server for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -38,9 +38,13 @@ app.use('/api/heatmap', heatmapRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
-);
+if (!process.env.VERCEL) {
+  app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+}
+
+module.exports = app;
